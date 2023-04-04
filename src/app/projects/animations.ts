780f2b3793d72c511eeaba854aaad935deb4aa81
Alvaro Mateo <lunadepluton322@gmail.com>
2023-04-04 10:42:14 -0500

    changing styles with animations

--- src/app/projects/animations.ts
+++ src/app/projects/animations.ts
@@ -1,11 +1,35 @@
-import { trigger, state, style } from '@angular/animations';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 export const markedTrigger = trigger('markedState',[
   state('default',style({
     border: '1px solid black',
-    backgroundColor: 'transparent'
-  })),state('marked',style({
-    border: '2px solid blue',
-    backgroundColor: '#caeff9'
+    backgroundColor: 'transparent',
+    padding: '20px'
   })),
+  state('marked',style({
+    border: '2px solid blue',
+    backgroundColor: '#caeff9',
+    padding: '19px'
+  })),
+  transition('default => marked', [ //steps
+    style({
+      border: '2px solid black',
+      padding: '19px'
+    }),
+    //transicion de escala
+    animate('200ms ease-out', style({ //transicion temporal
+      transform: 'scale(1.05)'
+    })),
+    //tiempo de la transicion
+    animate(200)
+  ]),
+  transition('marked => default', [ //steps
+    style({
+      border: '1px solid blue',
+      padding: '20px'
+    }),
+    animate('300ms ease-out')
+  ]),
+  // transition('default <=> marked', animate('300ms ease-out')),
+  // transition('default => marked', animate('300ms ease-out'))
 ]);
